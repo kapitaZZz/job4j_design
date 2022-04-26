25db858e14c9258fbda7faffb66cb59d08d18975
KapitaZzz <agarussky@gmail.com>
2022-04-27 00:12:23 +0300

    1. Реализовать коллекцию Set на массиве [#996 #293576]

--- src/main/java/ru/job4j/User.java
+++ src/main/java/ru/job4j/User.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,39 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Steve", 1, new GregorianCalendar(1988, Calendar.NOVEMBER, 25, 10, 15, 11));
+        User user2 = new User("Steve", 1, new GregorianCalendar(1988, Calendar.NOVEMBER, 25, 10, 15, 11));
+
+        Map<User, Object> userObjectMap = new HashMap<>();
+        userObjectMap.put(user1, new Object());
+        userObjectMap.put(user2, new Object());
+
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+        System.out.println(user1.equals(user2));
+
+        for (Map.Entry<User, Object> u : userObjectMap.entrySet()) {
+            System.out.println(u.getKey() + " - " + u.getValue());
+        }
     }
 }
