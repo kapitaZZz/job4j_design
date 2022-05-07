d9d4aae0c2355a0fac0464866a1a19d7e2c79515
KapitaZzz <agarussky@gmail.com>
2022-05-07 17:04:59 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,9 +3,21 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        Path path = Paths.get("C:\\projects\\job4j_design\\");
+        List<Path> pathList = getPath(path);
+        pathList.forEach(System.out::println);
+
     }
+
+    public static List<Path> getPath(Path path) throws IOException {
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(path, duplicatesVisitor);
+        return duplicatesVisitor.showPathList();
+    }
+
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,25 +1,42 @@
 package ru.job4j.io.duplicates;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private final Set<FileProperty> files = new HashSet<>();
+    private final Map<FileProperty, List<Path>> propertyListMap = new HashMap<>();
 
-    @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty fileProperty = new FileProperty(Files.size(file), file.getFileName().toString());
-        if (!files.add(fileProperty)) {
-            System.out.println(fileProperty.getName() + " - is a duplicate");
+        FileProperty fileProperty = new FileProperty(Files.size(file), file.toFile().getName());
+        if (propertyListMap.containsKey(fileProperty)) {
+            List<Path> pathList = propertyListMap.get(fileProperty);
+            pathList.add(file.toAbsolutePath());
+            propertyListMap.put(fileProperty, pathList);
+        } else {
+            List<Path> prevPathList = new ArrayList<>();
+            prevPathList.add(file.toAbsolutePath());
+            propertyListMap.put(fileProperty, prevPathList);
         }
         return super.visitFile(file, attrs);
     }
+
+    public List<Path> showPathList() {
+        List<Path> pathList = new ArrayList<>();
+        for (List<Path> list : propertyListMap.values()) {
+            if (list.size() > 1) {
+                pathList.addAll(list);
+            }
+        }
+        return pathList;
+    }
 }
+
