035cc2c09cf3c17fba67c9b705933025a9abee63
KapitaZzz <agarussky@gmail.com>
2022-05-18 18:50:29 +0300

    7. Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class CSVReader {
+
+    public static void handle(ArgsName argsName) throws IOException {
+        Path source = Path.of(argsName.get("path"));
+        String delimiter = argsName.get("delimiter");
+        String target = argsName.get("out");
+        String[] filter = argsName.get("filter").split(",");
+        StringBuilder builder = new StringBuilder();
+        String[] nameLine = null;
+        try (Scanner scanner = new Scanner(source).useDelimiter(System.lineSeparator())) {
+            if (scanner.hasNext()) {
+                nameLine = scanner.nextLine().split(delimiter);
+            }
+            assert nameLine != null;
+            int[] positions = new int[filter.length];
+            int count = 0;
+            for (String s : filter) {
+                builder.append(s + ";");
+                for (int j = 0; j < nameLine.length; j++) {
+                    if (s.equals(nameLine[j])) {
+                        positions[count++] = j;
+                    }
+                }
+            }
+            builder.deleteCharAt(builder.length() - 1);
+            builder.append(System.lineSeparator());
+            while (scanner.hasNext()) {
+                String[] array = scanner.nextLine().split(delimiter);
+                for (int position : positions) {
+                    builder.append(array[position] + ";");
+                }
+                builder.deleteCharAt(builder.length() - 1);
+                builder.append(System.lineSeparator());
+            }
+            if (target.equals("stdout")) {
+                System.out.println(builder);
+            } else {
+                try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(target))) {
+                    printWriter.print(builder);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
