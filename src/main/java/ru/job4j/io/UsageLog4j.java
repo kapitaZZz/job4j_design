49d122427a84060673a98ba87dcd8aed84f3ae78
KapitaZzz <agarussky@gmail.com>
2022-05-18 20:26:24 +0300

    2. Simple Loggin Facade 4 Java. [#268849 #302396]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,23 +7,10 @@ public class UsageLog4j {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
     public static void main(String[] args) {
-        String name = "Sam Fisher";
-        int age = 33;
-        boolean isMarried = false;
-        float height = 1.72f;
-        double salary = 45000.50;
-        long personId = 41235874513L;
-        byte childrenCount = 1;
-        short workExperience = 15;
-        LOG.debug("User info name : {}, age : {}, height : {}, salary per year : {}, personal ID : {},"
-                        + " child(children) : {}, work experience : {}, is married : {}",
-                name,
-                age,
-                height,
-                salary,
-                personId,
-                childrenCount,
-                workExperience,
-                isMarried);
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
     }
 }
