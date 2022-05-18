8d2d6462a9fd8297aa221f8622b9c7a77a9286fd
KapitaZzz <agarussky@gmail.com>
2022-05-18 21:24:10 +0300

    3. Формат XML [#313165]

---
+++ src/main/java/ru/job4j/serialization/xml/House.java
@@ -0,0 +1,33 @@
+package ru.job4j.serialization.xml;
+
+import ru.job4j.serialization.json.Worker;
+
+import java.util.Arrays;
+
+public class House {
+
+    private String address;
+    private int stages;
+    private boolean isComplete;
+    private Worker worker;
+    private String[] typeHouse;
+
+    public House(String address, int stages, boolean isComplete, Worker worker, String[] typeHouse) {
+        this.address = address;
+        this.stages = stages;
+        this.isComplete = isComplete;
+        this.worker = worker;
+        this.typeHouse = typeHouse;
+    }
+
+    @Override
+    public String toString() {
+        return "House{"
+                + "address='" + address + '\''
+                + ", stages=" + stages
+                + ", isComplete=" + isComplete
+                + ", worker=" + worker
+                + ", typeHouse=" + Arrays.toString(typeHouse)
+                + "}";
+    }
+}
