4c39d654d8bf9c10bae55cbb11a6693b8bea20a9
KapitaZzz <agarussky@gmail.com>
2022-05-20 11:55:59 +0300

    4. JAXB. Преобразование XML в POJO. [#315063 #302441]

--- src/main/java/ru/job4j/serialization/xml/Worker.java
+++ src/main/java/ru/job4j/serialization/xml/Worker.java
@@ -1,9 +1,12 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "worker")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Worker {
     @XmlAttribute
     private String name;
