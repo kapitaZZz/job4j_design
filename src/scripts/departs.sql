f19319efe733539a44172f6d0aadade751fa06d9
KapitaZzz <agarussky@gmail.com>
2022-05-25 11:52:25 +0300

    0. Join [#6862]

--- src/scripts/departs.sql
+++ src/scripts/departs.sql
@@ -1,31 +1,42 @@
-create table employees (
-	id serial primary key,
-	name varchar(255)
-);
-
 create table departments(
-	id serial primary key,
-	name varchar(255),
-	employee_id int references employees(id)
+    id serial primary key,
+    name varchar(255)
+);
+create table employees(
+    id serial primary key,
+    name varchar(255),
+    department_id int references departments(id)
 );
 
 insert into departments(name) values ('IT'), ('Marketing'), ('HR'), ('Sale');
 
 insert into employees(name) values ('Sam'), ('Thobias'), ('Helen'), ('Steve'), ('Margo');
 
-select * from employees e left join departments d on e.id = d.employee_id;
-select * from employees e right join departments d on e.id = d.employee_id;
-select * from employees e full join departments d on e.id = d.employee_id;
-select * from employees e cross join departments d;
+select * from departments d join employees e
+on d.id = e.department_id;
 
-select e.name, d.name as departament from departments d right join employees e
-on e.id = d.employee_id;
+select * from departments d left join employees e
+on d.id = e.department_id;
 
-select e.name, d.name as departament from employees e left join departments d
-on e.id = d.employee_id;
+select * from departments d right join employees e
+on d.id = e.department_id;
 
-select d.name from departments d left join employees e on e.id = d.employee_id
-where d.employee_id is null;
+select * from departments d full join employees e
+on d.id = e.department_id;
+
+select * from departments cross join employees;
+
+select e.name, d.name
+from departments d right join employees e
+on d.id = e.department_id;
+
+select e.name, d.name
+from employees e left join departments d
+on d.id = e.department_id;
+
+select d.name from departments d left join employees e
+on d.id = e.department_id
+where e.department_id is null;
 
 create table teens(
   name varchar(255),
