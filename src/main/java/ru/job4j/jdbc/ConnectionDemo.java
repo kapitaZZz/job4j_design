fed8f7543b9fa1d416fecaf384951433cd63c652
KapitaZzz <agarussky@gmail.com>
2022-05-26 07:51:10 +0300

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,22 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config("app.properties");
+        config.load();
+        config.value("jdbc.driver");
+        Connection connection = DriverManager.getConnection(config.value("jdbc.url"),
+                config.value("jdbc.login"),
+                config.value("jdbc.password"));
+        DatabaseMetaData metaData = connection.getMetaData();
+        System.out.println(metaData.getUserName());
+        System.out.println(metaData.getURL());
+    }
+}
